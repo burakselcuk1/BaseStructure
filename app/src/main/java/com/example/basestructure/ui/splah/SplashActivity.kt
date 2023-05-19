3440ebddc030837ccdae4f46a3b91806c79c857d
burak <buraksoftware1@gmail.com>
2023-05-19 10:55:21 +0300

    Splash added

---
+++ app/src/main/java/com/example/basestructure/ui/splah/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.basestructure.ui.splah
+
+import android.animation.Animator
+import android.content.Intent
+import com.airbnb.lottie.LottieAnimationView
+import com.example.basestructure.R
+import com.example.basestructure.base.BaseActivity
+import com.example.basestructure.databinding.ActivitySplashBinding
+import com.example.basestructure.ui.mainActivity.MainActivity
+
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(
+    viewModelClass = SplashViewModel::class.java,
+    layoutId = com.example.basestructure.R.layout.activity_splash
+) {
+
+
+    override fun onInitDataBinding() {
+        binding.animationView.setAnimation("progress/splash.json")
+        binding.animationView.setImageAssetsFolder("progress/")
+        binding.animationView.playAnimation()
+
+        binding.animationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                // Animasyon başladığında yapılacaklar
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                // Animasyon bittiğinde MainActivity'ye geçiş yapılıyor
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish() // Bu splash ekranını kapatır, böylece geri tuşuna basıldığında splash ekranına dönülmez.
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                // Animasyon iptal edildiğinde yapılacaklar
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                // Animasyon tekrarlandığında yapılacaklar
+            }
+        })
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/basestructure/ui/splah/SplashViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.basestructure.ui.splah
+
+import com.example.basestructure.base.BaseViewModel
+
+class SplashViewModel: BaseViewModel() {
+}
