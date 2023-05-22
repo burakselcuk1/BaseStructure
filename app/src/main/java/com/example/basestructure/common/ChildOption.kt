855e68e4b5466e92fea6d83e5c90311c7869e175
burak <buraksoftware1@gmail.com>
2023-05-22 15:36:21 +0300

    Added child options

---
+++ app/src/main/java/com/example/basestructure/common/ChildOption.kt
@@ -0,0 +1,21 @@
+package com.example.basestructure.common
+
+enum class ChildOption(val displayName: String) {
+    EV_KIRALAMA("Ev Kiralama"),
+    BILET_ALMA("Bilet Alma"),
+    RESTORAN_TAVSIYESI("Restoran Tevsiyesi"),
+    GEZILECEK_YERLER("Gezilecek Yerler"),
+    ISIM_URETICI("İsim Üretici"),
+    ILISKI_TAVSIYELERI("İlişki Tavsiyeleri"),
+    BASLIK_FIKIRLERI("Başlık Fikirleri"),
+    SIIR_YAZMA("Şiir Yazma"),
+    IS_ILANI("İş İlanı"),
+    HUCRE_ORGANELLERI("Hücre Organelleri"),
+    IKLIM_DEGISIKLIGI("İklim Değişikliği"),
+    EVRIM_TEORISI("Evrim Teorisi"),
+    SAC_UZATMAK("Saç Uzatmak"),
+    DAHA_IYI_UYKU("Daha iyi uyku"),
+    SABAH_RUTINI("Sabah Rutini"),
+    KITAP_ONERILERI("Kitap Önerileri"),
+    // Diğer seçenekler...
+}
--- app/src/main/java/com/example/basestructure/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/basestructure/ui/chat/ChatFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.basestructure.R
 import com.example.basestructure.adapter.MessageAdapter
 import com.example.basestructure.base.BaseFragment
+import com.example.basestructure.common.ChildOption
 import com.example.basestructure.databinding.FragmentChatBinding
 import com.example.chatgptapp.model.Message
 import com.google.android.material.internal.ViewUtils
@@ -60,9 +61,61 @@ class ChatFragment : BaseFragment<FragmentChatBinding, ChatViewModel>(
         // Get the clicked child name from the arguments
         val clickedChildName = arguments?.getString("clickedChildName")
 
-        // Display a toast message
-        Toast.makeText(requireContext(), "Clicked child: $clickedChildName", Toast.LENGTH_SHORT).show()
-
+        val clickedChildOption = ChildOption.values().find { it.displayName == clickedChildName }
+        when(clickedChildOption) {
+            ChildOption.EV_KIRALAMA -> {
+                binding.messageEditText.setText(getString(R.string.house_rent))
+            }
+            ChildOption.BILET_ALMA -> {
+                binding.messageEditText.setText(getString(R.string.take_ticket))
+            }
+            ChildOption.RESTORAN_TAVSIYESI -> {
+                binding.messageEditText.setText(getString(R.string.suggest_restaurant))
+            }
+            ChildOption.GEZILECEK_YERLER -> {
+                binding.messageEditText.setText(getString(R.string.travel))
+            }
+            ChildOption.ISIM_URETICI -> {
+                binding.messageEditText.setText(getString(R.string.name_crator))
+            }
+            ChildOption.ILISKI_TAVSIYELERI -> {
+                binding.messageEditText.setText(getString(R.string.suggest_relationship))
+            }
+            ChildOption.BASLIK_FIKIRLERI -> {
+                binding.messageEditText.setText(getString(R.string.suggest_title))
+            }
+            ChildOption.SIIR_YAZMA -> {
+                binding.messageEditText.setText(getString(R.string.write_document))
+            }
+            ChildOption.IS_ILANI -> {
+                binding.messageEditText.setText(getString(R.string.job_posts))
+            }
+            ChildOption.HUCRE_ORGANELLERI -> {
+                binding.messageEditText.setText(getString(R.string.cell))
+            }
+            ChildOption.IKLIM_DEGISIKLIGI -> {
+                binding.messageEditText.setText(getString(R.string.climate))
+            }
+            ChildOption.EVRIM_TEORISI -> {
+                binding.messageEditText.setText(getString(R.string.evolution))
+            }
+            ChildOption.SAC_UZATMAK -> {
+                binding.messageEditText.setText(getString(R.string.hair))
+            }
+            ChildOption.DAHA_IYI_UYKU -> {
+                binding.messageEditText.setText(getString(R.string.sleep))
+            }
+            ChildOption.SABAH_RUTINI -> {
+                binding.messageEditText.setText(getString(R.string.routine))
+            }
+            ChildOption.KITAP_ONERILERI -> {
+                binding.messageEditText.setText(getString(R.string.book))
+            }
+            else -> {
+                // Beklenmeyen bir durum
+                Toast.makeText(requireContext(), "Beklenmeyen bir seçenek", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
         binding.sendBtn.setOnClickListener {
