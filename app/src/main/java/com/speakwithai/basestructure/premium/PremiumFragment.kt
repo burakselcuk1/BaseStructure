7b93824466742b83233e99d96de97efdf50dd40a
burak selçuk <89164849+burakselcuk1@users.noreply.github.com>
2024-02-19 14:15:25 +0300

    Added premium animation

--- app/src/main/java/com/speakwithai/basestructure/premium/PremiumFragment.kt
+++ app/src/main/java/com/speakwithai/basestructure/premium/PremiumFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingResult
@@ -23,31 +24,37 @@ import dagger.hilt.android.AndroidEntryPoint
 import pl.droidsonroids.gif.GifDrawable
 import pl.droidsonroids.gif.GifImageView
 import javax.inject.Inject
+import android.animation.Animator
+import com.speakwithai.basestructure.databinding.FragmentPremiumBinding
 
 @AndroidEntryPoint
 class PremiumFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentPremiumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_premium, container, false)
-    }
+        binding = FragmentPremiumBinding.inflate(inflater, container, false)
+        return binding.root    }
     @Inject
     lateinit var billingManager: BillingManager
+    val premiumButton = view?.findViewById<GifImageView>(R.id.gifImageView)
+    var premiumItemsLayout = view?.findViewById<View>(R.id.premium_items)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        AnalyticsHelper.logScreenView("PremiumFragment","PremiumFragment",requireContext())
 
         val gifImageView = view.findViewById<GifImageView>(R.id.imageView5)
         val gifImageViewTwo = view.findViewById<GifImageView>(R.id.imageView7)
         val gifImageViewThree = view.findViewById<GifImageView>(R.id.imageView8)
-        val premiumButton = view.findViewById<GifImageView>(R.id.gifImageView)
-        val price = view.findViewById<TextView>(R.id.price)
+
         val gifDrawable = gifImageView.drawable as GifDrawable
         val gifDrawableTwo = gifImageViewTwo.drawable as GifDrawable
         val gifDrawableThree = gifImageViewThree.drawable as GifDrawable
+
+
+        AnalyticsHelper.logScreenView("PremiumFragment","PremiumFragment",requireContext())
 
         val handler = Handler()
         handler.postDelayed({
@@ -60,26 +67,57 @@ class PremiumFragment : BottomSheetDialogFragment() {
         val handler1 = Handler()
         handler1.postDelayed({
             if (gifDrawable.isRunning) {
-                price.visibility = View.VISIBLE
+                binding.price.visibility = View.VISIBLE
             }
         }, 1500)
 
-        premiumButton.applyClickShrink()
-        premiumButton.setOnClickListener {
+        binding.gifImageView.applyClickShrink()
+        binding.gifImageView.setOnClickListener {
             handleBillingProcess()
+
         }
-
-
-
-
     }
-
 
     private fun handleBillingProcess() {
         val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
             if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
                 // Satın alma başarılı
+                with(binding){
+                    imageView5.visibility = View.GONE
+                    imageView7.visibility = View.GONE
+                    imageView8.visibility = View.GONE
+                    price.visibility = View.GONE
+                    binding.gifImageView.visibility = View.GONE
+                    premiumText.visibility = View.GONE
+                    premiumButton?.visibility = View.GONE
+                    premiumItems.linear1.visibility = View.GONE
+                    premiumItems.linear2.visibility = View.GONE
+                }
+                with(binding){
+                    animationView.setAnimation("premiumanimation.json")
+                    animationView.playAnimation()
+                    animationView.addAnimatorListener(object : Animator.AnimatorListener {
+
+                        override fun onAnimationStart(p0: Animator) {
+                            premiumItemsLayout?.visibility = View.GONE
+
+                        }
+
+                        override fun onAnimationEnd(p0: Animator) {
+                            animationView.visibility = View.GONE
+                            price.visibility = View.GONE
+
+                        }
+
+                        override fun onAnimationCancel(p0: Animator) {
+                        }
+
+                        override fun onAnimationRepeat(p0: Animator) {
+                        }
+                    })
+                }
                 Toast.makeText(requireContext(), getString(R.string.premium_user), Toast.LENGTH_SHORT).show()
+
 
             } else {
                 // Satın alma başarısız
